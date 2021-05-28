a0abd0ca0536d0262785c404da59042270b31acc
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-28 17:34:19 +0400

    WS text-service prepared

---
+++ FTPBasedSystem.WindowsServices.TEXTSERVICE/PipelineHelpers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+
+namespace FTPBasedSystem.WindowsServices.TEXTSERVICE
+{
+    public static class PipelineHelpers
+    {
+        public static string ReadFromFtp(string directory, string credential)
+        {
+            if (directory is null) throw new ArgumentNullException(nameof(directory));
+            if (credential is null) throw new ArgumentNullException(nameof(credential));
+
+            var ftpIp = "ftp://127.0.0.1/" + directory;
+            var ftpRequest = (FtpWebRequest) WebRequest.Create(ftpIp);
+            ftpRequest.CachePolicy = new RequestCachePolicy(RequestCacheLevel.CacheIfAvailable);
+
+            ftpRequest.UseBinary = true;
+            ftpRequest.UsePassive = true;
+            ftpRequest.KeepAlive = false;
+            ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+            ftpRequest.Credentials = new NetworkCredential(credential, credential);
+
+            using var ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+            Console.WriteLine($"{directory} is downloaded via ftp | " +
+                              $"Status => Code: {ftpResponse.StatusCode}, " +
+                              $"Description: {ftpResponse.StatusDescription}");
+
+            using var responseStream = ftpResponse.GetResponseStream();
+            using var srcReader = new StreamReader(responseStream);
+            var fileContents = srcReader.ReadToEnd();
+            responseStream.Close();
+            srcReader.Close();
+            ftpResponse.Close();
+
+            return fileContents;
+        }
+
+        public static string SortAllRows(string tableText)
+        {
+            if (tableText == null) throw new ArgumentNullException(nameof(tableText));
+            return string.Empty;
+        }
+
+        public static bool EnqueueTheText(string result)
+        {
+            if (result == null) throw new ArgumentNullException(nameof(result));
+
+            // if successfully added to MQ then return true
+
+            // otherwise false
+
+            return true;
+        }
+    }
+}
--- FTPBasedSystem.WindowsServices.TEXTSERVICE/Program.cs
+++ FTPBasedSystem.WindowsServices.TEXTSERVICE/Program.cs
@@ -6,7 +6,9 @@ namespace FTPBasedSystem.WindowsServices.TEXTSERVICE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("WS -> Text Service is starting to watch proper directory..");
+            var watcher = new FtpDirectoryWatcher();
+            watcher.WatchDirectory();
         }
     }
 }
