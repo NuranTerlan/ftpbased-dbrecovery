3663397eae875d51dad0812bc89d1166d783dcac
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-29 17:14:48 +0400

    change WS entity watching service title

--- FTPBasedSystem.WindowsServices.TEXTSERVICE/Program.cs
+++ FTPBasedSystem.WindowsServices.TEXTSERVICE/Program.cs
@@ -6,6 +6,7 @@ namespace FTPBasedSystem.WindowsServices.TEXTSERVICE
     {
         static void Main(string[] args)
         {
+            Console.Title = "Text Service Analyzer";
             Console.WriteLine("WS -> Text Service is starting to watch proper directory..");
             var watcher = new FtpDirectoryWatcher();
             watcher.WatchDirectory();
