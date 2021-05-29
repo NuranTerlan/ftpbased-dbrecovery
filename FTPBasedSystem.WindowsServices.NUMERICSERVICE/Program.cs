1a286c4514d24e65ba1bc5724e5a76489df76d88
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-29 17:14:36 +0400

    change WS entity watching service title

--- FTPBasedSystem.WindowsServices.NUMERICSERVICE/Program.cs
+++ FTPBasedSystem.WindowsServices.NUMERICSERVICE/Program.cs
@@ -6,6 +6,7 @@ namespace FTPBasedSystem.WindowsServices.NUMERICSERVICE
     {
         static void Main(string[] args)
         {
+            Console.Title = "Numeric Service Analyzer";
             Console.WriteLine("WS -> Numeric Service is starting to watch proper directory..");
             var watcher = new FtpDirectoryWatcher();
             watcher.WatchDirectory();
