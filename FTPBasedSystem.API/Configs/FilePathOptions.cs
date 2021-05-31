b0ba93229005922f851598d0bd670325eda761e0
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-31 17:18:22 +0400

    remove redundant properties

--- FTPBasedSystem.API/Configs/FilePathOptions.cs
+++ FTPBasedSystem.API/Configs/FilePathOptions.cs
@@ -5,10 +5,6 @@
         public string LocalDate { get; set; }
         public string LocalText { get; set; }
         public string LocalNumeric { get; set; }
-        public string FtpDate { get; set; }
-        public string FtpText { get; set; }
-        public string FtpNumeric { get; set; }
         public string TempDataFolder { get; set; }
-        public string FtpHostName { get; set; }
     }
 }
