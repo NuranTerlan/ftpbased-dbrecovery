c1b7cf2a9a8e14ad6d8e013d6181fc7c7dae9593
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-31 04:35:29 +0400

    model for using file paths config

---
+++ FTPBasedSystem.API/Configs/FilePathOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace FTPBasedSystem.API.Configs
+{
+    public class FilePathOptions
+    {
+        public string LocalDate { get; set; }
+        public string LocalText { get; set; }
+        public string LocalNumeric { get; set; }
+        public string FtpDate { get; set; }
+        public string FtpText { get; set; }
+        public string FtpNumeric { get; set; }
+        public string TempDataFolder { get; set; }
+    }
+}
