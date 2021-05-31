7be8b470e7e22975fe2d447cd7329031f3567ec7
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-31 04:39:00 +0400

    return bool to know whether web request successful or not to do something in future correctly (and create transaction logic as code)

--- FTPBasedSystem.SERVICES/FtpServices/Concretion/FtpHelpers.cs
+++ FTPBasedSystem.SERVICES/FtpServices/Concretion/FtpHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,31 +17,41 @@ namespace FTPBasedSystem.SERVICES.FtpServices.Concretion
             _logger = logger;
         }
 
-        public async Task UploadFile(string username, string password, string from, string to)
+        public async Task<bool> UploadFile(string username, string password, string from, string to)
         {
-            var ftpIp = "ftp://127.0.0.1/" + to;
-            var ftpRequest = (FtpWebRequest) WebRequest.Create(ftpIp);
-            ftpRequest.UseBinary = true;
-            ftpRequest.UsePassive = true;
-            ftpRequest.KeepAlive = false;
-            ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
-            ftpRequest.Credentials = new NetworkCredential(username, password);
+            try
+            {
+                var ftpIp = "ftp://127.0.0.1/" + to;
+                var ftpRequest = (FtpWebRequest) WebRequest.Create(ftpIp);
+                ftpRequest.UseBinary = true;
+                ftpRequest.UsePassive = true;
+                ftpRequest.KeepAlive = false;
+                ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                ftpRequest.Credentials = new NetworkCredential(username, password);
 
-            using var srcReader = new StreamReader(from);
-            var fileContents = Encoding.UTF8.GetBytes(await srcReader.ReadToEndAsync());
-            srcReader.Close();
+                using var srcReader = new StreamReader(from);
+                var fileContents = Encoding.UTF8.GetBytes(await srcReader.ReadToEndAsync());
+                srcReader.Close();
 
-            ftpRequest.ContentLength = fileContents.Length;
+                ftpRequest.ContentLength = fileContents.Length;
 
-            await using var requestedStream = ftpRequest.GetRequestStream();
-            await requestedStream.WriteAsync(fileContents, 0, fileContents.Length);
-            requestedStream.Close();
+                await using var requestedStream = ftpRequest.GetRequestStream();
+                await requestedStream.WriteAsync(fileContents, 0, fileContents.Length);
+                requestedStream.Close();
 
-            using var ftpResponse = (FtpWebResponse) await ftpRequest.GetResponseAsync();
-            _logger.LogInformation($"{from} is uploaded as ({to}) via ftp | " +
-                                   $"Status => Code: {ftpResponse.StatusCode}, " +
-                                   $"Description: {ftpResponse.StatusDescription}");
-            ftpResponse.Close();
+                using var ftpResponse = (FtpWebResponse) await ftpRequest.GetResponseAsync();
+                _logger.LogInformation($"{from} is uploaded as ({to}) via ftp | " +
+                                       $"Status => Code: {ftpResponse.StatusCode}, " +
+                                       $"Description: {ftpResponse.StatusDescription}");
+                ftpResponse.Close();
+                return true;
+            }
+            catch (WebException e)
+            {
+                var status = ((FtpWebResponse)e.Response)?.StatusDescription;
+                _logger.LogError(e, status);
+                return false;
+            }
         }
     }
 }
