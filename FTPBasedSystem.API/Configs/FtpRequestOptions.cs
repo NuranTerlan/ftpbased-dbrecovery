fece9bd77b4f4460efa728782e64740c3015c011
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-31 10:57:22 +0400

    ftp request settings to bind configuration and use it in classes

---
+++ FTPBasedSystem.API/Configs/FtpRequestOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace FTPBasedSystem.API.Configs
+{
+    public class FtpRequestOptions
+    {
+        public string HostName { get; set; }
+        public string NumericCredential { get; set; }
+        public string TextCredential { get; set; }
+        public string DateCredential { get; set; }
+    }
+}
