0c8a0b0ce6723494a94157dc4b7d04bbbaaed6f5
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-31 04:35:15 +0400

    model for using cron config

---
+++ FTPBasedSystem.API/Configs/CronOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace FTPBasedSystem.API.Configs
+{
+    public class CronOptions
+    {
+        public string EverySeconds { get; set; }
+        public string EveryMinutes { get; set; }
+        public string EveryHours { get; set; }
+        public string EveryDays { get; set; }
+        public string EveryMonths { get; set; }
+    }
+}
