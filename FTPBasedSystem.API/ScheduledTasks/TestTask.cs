0c764230904d2966ed353f904fdbb640b1cfcecf
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-01 11:41:59 +0400

    testtask

---
+++ FTPBasedSystem.API/ScheduledTasks/TestTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace FTPBasedSystem.API.ScheduledTasks
+{
+    public static class TestTask
+    {
+        private static readonly object Flag = new object();
+
+        public static void DoSomething()
+        {
+            lock (Flag)
+            {
+                for (var i = 0; i < 8; i++)
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
